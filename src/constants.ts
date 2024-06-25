eb9c29bcc881e0f17ff0694a5bae7ced27766928
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-06-25 22:35:23 +0100

    Organise constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,14 @@
 export const translationsFolderPath = "C:/Program Files (x86)/Steam/steamapps/common/SCP Secret Laboratory/Translations";
-export const workDirectoryPath = "work";
-export const translationsOutputPath = workDirectoryPath + "/Translations";
-export const dataFolderPath = "data";
-export const scp914RecipesFilePath = dataFolderPath + "/scp-914-recipes.json";
+
 export const itemsFilePath = "/Items.txt";
 export const classDescriptionsFilePath = "/Class_Descriptions.txt"
 export const facilityFilePath = "/Facility.txt"
-export const youAreTextIndex = 31;
+
+export const workDirectoryPath = "work";
+export const dataFolderPath = "data";
+export const translationsOutputPath = workDirectoryPath + "/Translations";
+export const scp914RecipesFilePath = dataFolderPath + "/scp-914-recipes.json";
+
 export const invisibleCharacter = "ㅤ"
+
+export const youAreTextIndex = 31;
